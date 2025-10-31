c759ad9666b7fcab68e86f67ac489e352ed84e4a
Carlos Zerpa <c.zerpa@uniandes.edu.co>
2025-10-31 18:11:02 -0500

    added vehicles total by brands

--- src/app/vehicle/vehicle.component.ts
+++ src/app/vehicle/vehicle.component.ts
@@ -10,12 +10,28 @@ import { VehicleService } from './vehicle.service';
 export class VehicleComponent implements OnInit {
 
   vehicles: Array<Vehicle> = []
+  vehiclesByBrand: Record<string, number> = {}
+  objectKeys = Object.keys;
+
   constructor(private vehicleService: VehicleService) { }
   
   getVehicles(){
-    this.vehicleService.getVehicles().subscribe(vehicles => 
-      this.vehicles = vehicles
-    );
+    this.vehicleService.getVehicles().subscribe({
+      next: (vehicles) => {
+        this.vehicles = vehicles
+        this.getVehiclesByBrand()
+      },
+      error: (error) => {
+        console.error('Error getting vehicles!', error);
+      }
+    });
+  }
+
+  getVehiclesByBrand(){
+    this.vehiclesByBrand = {};
+    for (const vehicle of this.vehicles) {
+      this.vehiclesByBrand[vehicle.marca] = (this.vehiclesByBrand[vehicle.marca] || 0) + 1;
+    }
   }
 
   ngOnInit(): void {
